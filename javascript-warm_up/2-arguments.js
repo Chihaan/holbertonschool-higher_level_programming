0d108513c2a86356d962ceaf0ee0043ed7b44251
Chihaan <gavet.vadim@gmail.com>
2026-07-01 12:45:29 +0200

    add: file 2 Node

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,9 +1,13 @@
 #!/usr/bin/node
 
+const messOne = 'No argument';
+const messTwo = 'Argument found';
+const messThree = 'Arguments found';
+
 if (process.argv.length <= 2) {
-  console.log('No argument');
+  console.log(messOne);
 } else if (process.argv.length === 3) {
-  console.log('Argument found');
+  console.log(messTwo);
 } else {
-  console.log('Arguments found');
+  console.log(messThree);
 }
